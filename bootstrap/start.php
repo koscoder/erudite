f01f331a7d715b140ef04e07d7ab1f9ff0a46740
VadimIlyasov <vadim@codetiburon.com>
2014-12-08 00:11:00 +0200

    Vadim's config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead','kos-VirtualBox'),
-        'owner' => array('ct-hero')
+        'owner' => array('ct-hero'),
+        'vadim' => array('Vadims-MacBook-Pro.local')
 
 ));
 
