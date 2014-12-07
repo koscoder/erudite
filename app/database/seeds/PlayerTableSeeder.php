f30894ee178f991c1d53084f19b96507e6a0a93f
koscoder <pogrebnoj.konstantin@gmail.com>
2014-12-07 23:04:34 +0200

    Catch exception in table seeder

--- app/database/seeds/PlayerTableSeeder.php
+++ app/database/seeds/PlayerTableSeeder.php
@@ -13,11 +13,15 @@ class PlayerTableSeeder extends Seeder {
 
 		foreach(range(1, 10) as $index)
 		{
-			Player::create([
-                           'player_id' => $faker->numberBetween(1,3),
-                           'game_id'   => $faker->numberBetween(1,10),
-                           'score'     => $faker->randomElement(['0', '100', '300']),
-			]);
+			try {
+                            Player::create([
+                               'player_id' => $faker->numberBetween(1,3),
+                               'game_id'   => $faker->numberBetween(1,10),
+                               'score'     => $faker->randomElement(['0', '100', '300']),
+                            ]);
+                        } catch (QueryException $e) {
+                            // do nothing
+                        }
 		}
 	}
 
