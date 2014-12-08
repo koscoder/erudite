a9cc714d6ac9c2d3a82e6a503ed0d4c63a8b262b
VadimIlyasov <vadim@codetiburon.com>
2014-12-08 06:29:36 +0200

    testing another way

---
+++ frontend/source/static/assets/js/main.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+    function loadGames()
+    {
+        var url = '/api/game/game-options';
+        $.get(url, {}, function(list) {
+            $('#games-table tbody').empty();
+            $(list).each(function(index, item) {
+                var row = '<tr><td>'+item.title+'44</td>'
+                            +'<td>'+item.status+'</td>'
+                            +'<td>'+item.players.length+'/'+item.playersMax+'</td>'
+                            +'<td>'+item.room+'</td>'
+                            +'<td>[Films]</td>'
+                            +'<td><i class="ua flag"></i> <i class="belarus flag"></i> <i class="ru flag"></i></td>'
+                            +'<td><button class="button ui green" onclick="loadGame('+item.id+');">Join</button></td>'
+                            +'</tr>';
+                $('#games-table tbody').append(row);
+            });
+        });
+    }
+    
+    loadGames();
+});
